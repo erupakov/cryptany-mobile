93fc3adf774fc214b3982b603a893551c5952700
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-09-12 17:42:14 +0300

    Added controller and CC check routine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,6 @@
             }
             .form-pane {
                 background-color: white;
-                padding: 1em;
                 border: 1px solid white;
                 border-radius: 8px;
             }
@@ -29,9 +28,37 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12 col-md-8 ml-md-2 col-lg-6 ml-lg-3">
-                    <div class="form-pane">
+                <div class="col-11 col-sm-10 col-md-8 col-lg-6 form-pane p-3 mx-auto mt-3 ">
                     <form>
+    <div class="input-group" style="margin-bottom: 1em;">
+      <input type="number" name="srcAmount" class="form-control" aria-label="Transfer this amount of ETH" value="0.00254" min="0" max="1" required="required">
+      <div class="input-group-btn">
+        <button type="button" name="switchSrc" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          ETN
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <a class="dropdown-item" href="#">BTC</a>
+          <a class="dropdown-item" href="#">LTC</a>
+          <a class="dropdown-item" href="#">ETN</a>
+          <div role="separator" class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">WVS</a>
+        </div>
+      </div>
+    </div>
+    <div class="input-group">
+      <input type="number" name="dstAmount" class="form-control" aria-label="Get this amount of $" placeholder="$ to get" value="1" min="0" max="1" required="required">
+      <div class="input-group-btn">     
+        <button type="button" name="switchDst" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          USD
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <a class="dropdown-item" href="#">USD</a>
+          <a class="dropdown-item" href="#">EUR</a>
+          <a class="dropdown-item" href="#">GBP</a>
+        </div>
+      </div>
+    </div>
+
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -40,9 +67,8 @@
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-success btn-block">Submit</button>
                     </form>
-                    </div>
                 </div>
             </div>       
         </div>
