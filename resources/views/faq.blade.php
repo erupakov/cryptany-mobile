456975196cd969ca0832a5fec0ce5de969b239b5
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-09-22 17:33:37 +0300

    Updated FAQ

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,12 +3,18 @@
 @section('content')
 <h3>Frequenty asked questions</h3>
 <p class="lead">We know there will be a lot of questions regarding our service. We've prepared answers to some commonly asked questions for you</p>
-<h4>Question 1</h4>
+<h4>1. How to check a transaction status?</h4>
 <p class="lead">Answer to question 1</p>
-<h4>Question 2</h4>
+<h4>2. Do you store cryptocurrency in a wallet?</h4>
 <p class="lead">Answer to question 2</p>
-<h4>Question 3</h4>
-<p class="lead">Answer to question 2</p>
-<h4>Question ...</h4>
+<h4>3. Can I transfer more than 1 USD?</h4>
+<p class="lead">This application is made strictly for technology demonstration purposes 
+and thus we don't require complex registration and security checks that we will perform in full version app
+(in full accordance to current financial regulations). We respect anti-laundry laws and will never 
+allow our services to be involved in such kind of operations.</p>
+<h4>4. When I get fiat on my card?</h4>
+<p class="lead">Due to complex nature of international banking systems it can take from several 
+seconds to 7 days since we receive your payment. Most common time to wait for your fiats is 1 business day.</p>
+<h4>5. Why I can send only Ethereum?</h4>
 <p class="lead">Answer to question ...</p>
 @endsection
