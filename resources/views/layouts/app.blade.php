ed4db61269535677f1a9d758e76849ea93b48b2f
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-09-23 14:30:44 +0300

    Made redirect to HTTPS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+		<base href="https://mobile.cryptany.io/" />
         <title>{{ __('welcome.page_title') }}</title>
 
         <!-- Fonts -->
