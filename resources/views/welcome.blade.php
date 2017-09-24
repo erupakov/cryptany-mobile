21f3347f1785ff05fb591b4d734178627c9d3598
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-09-24 12:22:25 +0300

    Changed transaction to transfer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,5 +81,6 @@
 @endsection
 
 @section('add_js')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 <script src="js/payment_info.js"></script>
 @endsection
