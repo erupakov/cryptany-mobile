63c85b282e2ff4e642d4885f023ced2248f876a2
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-09-24 09:18:42 +0300

    Added notfound page

---
+++ resources/views/notfound.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<h3>Not found</h3>
+<p class="text-danger">Sorry, it seems that the requested page doesn't exist here. We'll try to find it later but for now please take a look at main page.</p>
+
+@endsection
