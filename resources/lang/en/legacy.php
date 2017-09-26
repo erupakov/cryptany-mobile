eb4f5eaa1eeccd1c136a2c136d5c3e57b5173aac
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-09-26 23:33:37 +0300

    Added legacy translations

---
+++ resources/lang/en/legacy.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'validity_date_placeholder' => 'Expiry date (MMYY)',
+    'email_placeholder' => 'Your email',
+    'first_name_placeholder' => 'First name',
+    'family_name_placeholder' => 'Family name',
+    'plastic_card_placeholder' => 'Card number (12 to 19 digits)',
+    'beta_version_warning' => 'Warning! In this alpha the maximum transaction amount is limited to 1 USD',
+    'submit' => 'Proceed',
+    'page_title' => 'New transaction'
+];
