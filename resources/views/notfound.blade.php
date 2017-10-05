51a412b122df22c0ddff24114090bdce7ed5b008
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-10-05 22:23:58 +0300

    Fixed typo

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <h3>Transaction not found</h3>
-<p class="text-danger">Sorry, we cannot find the requested transaction. Please check transaction ID to verify that all transaction infomration is correct.</p>
+<p class="text-danger">Sorry, we cannot find the requested transaction. Please check transaction ID to verify that all transaction information is correct.</p>
 @endsection
